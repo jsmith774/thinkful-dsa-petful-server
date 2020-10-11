4644bce2b06fa0d811e322cc556a20e47027bef4
Jim <jsmith774@gmail.com>
2020-10-11 13:08:10 -0400

    added dotenv

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { CLIENT_ORIGIN } = require('../config');
 const { NODE_ENV } = require('../config');
 
 const app = express();
 
-app.use(cors());
+app.use(cors);
 
 app.use('/people', require('../people/people.router'));
 app.use('/pets', require('../pets/pets.router'));
